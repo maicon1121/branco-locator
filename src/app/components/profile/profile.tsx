f1ed5595c6b513b5ff42095f665280e206b1e572
Maicon G. Passos <maicon.passos@diwe.com.br>
2023-10-21 18:40:19 -0300

    feat: testing components importing

---
+++ src/app/components/profile/profile.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+
+export default function Profile() {
+  return (
+    <Image
+      src="https://i.imgur.com/QIrZWGIs.jpg"
+      width={300}
+      height={300}
+      alt="Alan L. Hart"
+    />
+  );
+}
